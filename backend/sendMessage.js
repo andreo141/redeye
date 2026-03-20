1593599a2d0fff7d89b3b44a6c356e5c53bd31d5
andreo141 <andreo141mlg@gmail.com>
2026-03-20 04:56:15 +0100

    Add error handling for mqtt broker connectivity and telegram bot

--- backend/sendMessage.js
+++ backend/sendMessage.js
@@ -1,28 +1,52 @@
-import mqtt from 'mqtt'
+import mqtt from "mqtt";
 
-const client = mqtt.connect('mqtt://mosquitto:1883')
-const token = process.env.TELEGRAM_TOKEN
-const chatId = process.env.TELEGRAM_CHAT_ID
+const client = mqtt.connect("mqtt://mosquitto:1883");
+const token = process.env.TELEGRAM_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
 
-client.on('connect', () => {
-  client.subscribe('zigbee2mqtt/motion-sensor-1')
-})
+client.on("connect", () => {
+  console.log("Connected to MQTT broker");
+  client.subscribe("zigbee2mqtt/motion-sensor-1");
+});
 
-client.on('message', (topic, payload) => {
-  const data = JSON.parse(payload.toString())
-  
+client.on("close", function () {
+  console.log("Disconnected from MQTT broker");
+});
+
+client.on("error", function (error) {
+  console.error(`MQTT Error: ${error}`);
+});
+
+client.on("reconnect", function () {
+  console.log("Reconnecting to MQTT broker...");
+});
+
+client.on("message", (topic, payload) => {
+  const data = JSON.parse(payload.toString());
+
   if (data.occupancy === true) {
-    sendTelegram(`Motion detected in Bergkot at ${new Date().toLocaleTimeString('nl-BE')}!`)
+    sendTelegram(
+      `Motion detected in Bergkot at ${new Date().toLocaleTimeString("nl-BE")}!`,
+    );
   }
-})
+});
 
 async function sendTelegram(message) {
-  await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: message
-    })
-  })
+  try {
+    const res = await fetch(
+      `https://api.telegram.org/bot${token}/sendMessage`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: message,
+        }),
+      },
+    );
+
+    if (!res.ok) console.error("Telegram error:", await res.text());
+  } catch (err) {
+    console.error("Failed to send Telegram message.");
+  }
 }
