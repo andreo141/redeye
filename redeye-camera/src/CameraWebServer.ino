5d6ba03e7dd5dc751fa770ba8e156a6572b1e790
andreo141 <andreo141mlg@gmail.com>
2026-04-05 03:37:21 +0200

    stop using mDNS

--- redeye-camera/src/CameraWebServer.ino
+++ redeye-camera/src/CameraWebServer.ino
@@ -1,6 +1,5 @@
 #include "esp_camera.h"
 #include <WiFi.h>
-#include <ESPmDNS.h>
 
 //
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
@@ -153,16 +152,6 @@ void setup() {
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
 
-    if (!MDNS.begin("redeye-cam")) {
-        Serial.println("Error setting up MDNS responder!");
-        while(1) {
-            delay(1000);
-        }
-    }
-    Serial.println("mDNS responder started");
-
-    // Add service to MDNS-SD
-    MDNS.addService("http", "tcp", 80);
 
 }
 
