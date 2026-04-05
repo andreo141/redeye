3bc3d23f80e79ed6e091da020e2d38bd9d2c2e52
andreo141 <andreo141mlg@gmail.com>
2026-04-05 06:26:45 +0200

    refactor cooldown helper

--- backend/helpers/isCoolingDown.js
+++ backend/helpers/isCoolingDown.js
@@ -1,5 +1,5 @@
 let lastAlert = 0;
-const COOLDOWN_MS = process.env.COOLDOWN_MS ?? 60000;
+const COOLDOWN_MS = Number(process.env.CAMERA_TIMEOUT_MS) ?? 5000;
 
 export default function isCoolingDown() {
   const now = Date.now();
