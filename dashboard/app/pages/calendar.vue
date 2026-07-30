832c741b264b926ade4d5e1008c33c0c1eea03f1
andreo141 <andreo141mlg@gmail.com>
2026-07-31 01:49:24 +0200

    feat: initial occupancy calendar

--- dashboard/app/pages/calendar.vue
+++ dashboard/app/pages/calendar.vue
@@ -1,3 +1,26 @@
 <template>
-  <main></main>
+  <main>
+    <UCalendar
+      v-model="value"
+      range
+      :number-of-months="3"
+      :year-controls="false"
+      variant="subtle"
+    />
+  </main>
 </template>
+
+<script setup lang="ts">
+import { CalendarDate } from '@internationalized/date'
+
+const today = new Date()
+
+const thisMonth = today.getUTCMonth() + 1
+const thisDay = today.getUTCDate()
+const thisYear = today.getUTCFullYear()
+
+const value = shallowRef({
+  start: new CalendarDate(thisYear, thisMonth, thisDay),
+  end: new CalendarDate(thisYear, thisMonth, thisDay),
+})
+</script>
