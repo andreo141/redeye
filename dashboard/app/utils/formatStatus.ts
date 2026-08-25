3858c1d52638e2b1605356c32a384f99900706a8
andreo141 <andreo141mlg@gmail.com>
2026-08-25 02:27:40 +0200

    refactor formatStatus util

--- dashboard/app/utils/formatStatus.ts
+++ dashboard/app/utils/formatStatus.ts
@@ -2,19 +2,30 @@ import type { SystemStatus } from "~/types/status";
 import { formatDate } from "./formatDate";
 
 export function formatStatus(status: SystemStatus) {
-  const armed = status.armed;
   const override = status.override;
+  const occupancy = status.occupancy;
+  const hasOverride = override !== null;
+  const hasOccupancy = occupancy !== null;
 
-  if (!override && armed) {
-    return "ARMED - All Clear";
-  } else if (!override && !armed && status.occupancy) {
-    return `DISARMED - Occupied by ${status.occupancy?.occupant_name.toUpperCase()} until ${formatDate(status.occupancy?.departure_date)}`;
-  } else if (override?.state === "armed") {
-    return `ARMED - Set during occupancy from ${status.occupancy?.occupant_name.toUpperCase()}`;
-  } else if (override?.expires_at) {
-    const overrideMinutesRemaining = Math.floor(
-      (new Date(override.expires_at).getTime() - Date.now()) / 60000 + 1,
-    );
-    return `DISARMED - Temporary for ${overrideMinutesRemaining} minutes`;
+  if (!hasOverride && !hasOccupancy) {
+    return "ARMED - All clear";
   }
+
+  if (!hasOverride && hasOccupancy) {
+    return `DISARMED - Occupied by ${occupancy.occupant_name} until ${formatDate(occupancy.departure_date)}`;
+  }
+
+  if (hasOverride && !hasOccupancy && override.expires_at) {
+    const minutesRemaining =
+      Math.floor(
+        (new Date(override.expires_at).getTime() - Date.now()) / 60000,
+      ) + 1;
+    return `DISARMED - ${minutesRemaining} minutes remaining`;
+  }
+
+  if (hasOverride && hasOccupancy) {
+    return `ARMED - Set during occupancy from ${occupancy.occupant_name}`;
+  }
+
+  return "Something went wrong while fetching the status of the property";
 }
